552f312d8795bb4c4ceb957ca3d20a1cf70d9676
Liang Yu Chen <andymememe@users.noreply.github.com>
2021-01-16 12:29:01 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+enum RadioMessage {
+    message1 = 49434
+}
+input.onButtonPressed(Button.A, function () {
+    music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+})
+basic.forever(function () {
+	
+})
